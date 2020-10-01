590f400c76a6cbf01a92e80dc48c52961317a60a
Sinclair Guthrie <sinclair.guthrie@gmail.com>
2020-10-01 04:12:54 -0400

    adjusted algorithm to include game result (win/loss)

--- algo.js
+++ algo.js
@@ -30,9 +30,21 @@ const matchVal = function(data, privateUid) {
     if (data.queueId !== 400 && data.queueId !== 420 && data.queueId !== 430 && data.queueId !== 440) {
         return "Unsupported gametype"
     }
+    let response = {
+        result: null,
+        kdaDiff: null
+    }
     let partId = data.participantIdentities.find(x => x.player.accountId == privateUid).participantId;
     let partInfo = data.participants.find(x => x.participantId == partId);
     let teamId = partInfo.teamId;
+    // setting result on response object
+    if (teamId === 100) {
+        response.result = data.teams[0].win;
+    }
+    if (teamId === 200) {
+        response.result = data.teams[1].win;
+    }
+
     let playerRole;
     let enemyInfo;
     // playerRole is taking the value from one of two fields in riot's data: "role" or "lane". values are: MIDDLE, TOP, JUNGLE, DUO_CARRY, DUO_SUPPORT
@@ -50,13 +62,16 @@ const matchVal = function(data, privateUid) {
     if (playerRole === undefined || enemyInfo === undefined) {
         return "Can't parse the data - someone likely roamed a bunch";
     }
+
+    // calculating KDAs for summoner and their lane opponent to be set on response object
     let partKDA = (partInfo.stats.kills + partInfo.stats.assists) / partInfo.stats.deaths;
     let enemyKDA = (enemyInfo.stats.kills + enemyInfo.stats.assists) / enemyInfo.stats.deaths;
     if (partKDA === Infinity && enemyKDA === Infinity) return "Both perfect KDA"; // testing if both kda's are perfect
     let diff = partKDA - enemyKDA;
-    if (diff === Infinity) return "Perfect KDA";
-    if (diff === -Infinity) return "Enemy perfect KDA"
-    return (diff);
+    if (diff === Infinity) response.kdaDiff = "Perfect KDA";
+    else if (diff === -Infinity) response.kdaDiff = "Enemy perfect KDA";
+    else response.kdaDiff = diff;
+    return response;
 }
 
 exports.matchVal = matchVal;
